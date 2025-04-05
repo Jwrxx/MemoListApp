76cae0aafa673cee67ee046b7a11d339ab72bfe4
Ian McIntosh <ianamcintosh108@gmail.com>
2025-04-04 22:50:30 -0400

    Partial XML code

---
+++ app/src/main/java/com/example/memolistapp/DatePickerDialog.java
@@ -0,0 +1,59 @@
+package com.example.memolistapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class DatePickerDialog {
+    Calendar selectedDate;
+    public interface SaveDateListener {
+        void didFinishDatePickerDialog(Calendar selectedTime);
+    }
+    public DatePickerDialog(){
+        //Empty constructor required for DialogFragment
+    }
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.select_date, container);
+        getDialog().setTitle("Select Date");
+        selectedDate = Calendar.getInstance();
+        final CalendarView cv = view.findViewById(R.id.calendarView);
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+                selectedDate.set(Calendar.YEAR, year);
+                selectedDate.set(Calendar.MONTH, month);
+                selectedDate.set(Calendar.DAY_OF_MONTH, day);
+                selectedDate.set(Calendar.HOUR_OF_DAY, 0);
+                selectedDate.set(Calendar.MINUTE, 0);
+                selectedDate.set(Calendar.SECOND, 0);
+                selectedDate.set(Calendar.MILLISECOND, 0);
+            }
+        });
+        Button saveButton = view.findViewById(R.id.buttonSelect);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveItem(selectedDate);
+            }
+        });
+        Button cancelButton = view.findViewById(R.id.buttonCancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDialog().dismiss();
+            }
+        });
+        return view;
+    }
+    private void saveItem (Calendar selectedTime) {
+        SaveDateListener activity = (SaveDateListener) getActivity();
+        activity.didFinishDatePickerDialog(selectedTime);
+        getDialog().dismiss();
+    }
+}
+}
