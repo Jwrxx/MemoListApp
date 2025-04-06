5930f93f6f4c8eaebc2c8c6a2216da5c710635f6
johnwjr <jwalkjr115@gmail.com>
2025-04-06 00:12:08 -0400

    - added and called buttons in main activity - created MemoListActivity

--- app/src/main/java/com/example/memolistapp/MainActivity.java
+++ app/src/main/java/com/example/memolistapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.memolistapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        initListButton();
+        initSettingsButton();
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initListButton() {
+        ImageButton listButton = findViewById(R.id.imageButtonList);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the MemoListActivity when the button is clicked
+                Intent intent = new Intent(MainActivity.this, MemoListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton(){
+        ImageButton settingsButton = findViewById(R.id.imageButtonSettings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Start the MemoSettingsActivity when the button is clicked
+                Intent intent = new Intent(MainActivity.this, MemoSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
         });
     }
 }
