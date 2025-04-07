4381896b6ead15e9a72b1927a135e948045bc68d
johnwjr <jwalkjr115@gmail.com>
2025-04-07 19:20:18 -0400

    Called and implemented initMemoSettings to initialize radio buttons based on saved sort type and order from SharedPreferences

--- app/src/main/java/com/example/memolistapp/MemoSettingsActivity.java
+++ app/src/main/java/com/example/memolistapp/MemoSettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,12 +21,15 @@ public class MemoSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_memo_settings);
         initListButton();
         initSettingsButton();
+        initMemoSettings();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
     private void initListButton() {
         ImageButton listButton = findViewById(R.id.imageButtonList);
         listButton.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +43,41 @@ public class MemoSettingsActivity extends AppCompatActivity {
     }
 
     private void initSettingsButton() {
-        ImageButton settingsButton = findViewById(R.id.imageButtonSettings);
-        settingsButton.setEnabled(false);
+        ImageButton listButton = findViewById(R.id.imageButtonSettings);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemoSettingsActivity.this, MemoListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
 
+    private void initMemoSettings() {
+
+        String sortMemosBy = getSharedPreferences("MemoSettings", MODE_PRIVATE).getString("sortMemosBy", "priority");
+        String sortOrder = getSharedPreferences("MemoSettings", MODE_PRIVATE).getString("sortOrder", "ascending");
+
+        RadioButton radioButtonPriority = findViewById(R.id.radioButtonPriority);
+        RadioButton radioButtonDate = findViewById(R.id.radioButtonDate);
+        RadioButton radioButtonSubject = findViewById(R.id.radioButtonSubject);
+
+        if (sortMemosBy.equals("priority")) {
+            radioButtonPriority.setChecked(true);
+        } else if (sortMemosBy.equals("date")) {
+            radioButtonDate.setChecked(true);
+        } else {
+            radioButtonSubject.setChecked(true);
+        }
+
+        RadioButton radioAscending = findViewById(R.id.radioAscending);
+        RadioButton radioDescending = findViewById(R.id.radioDescending);
+
+        if (sortOrder.equals("ascending")) {
+            radioAscending.setChecked(true);
+        } else {
+            radioDescending.setChecked(true);
+        }
     }
 }
